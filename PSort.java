c8a320a6ca1eeb8ebe5572775506a478be912d08
unknown <bkool12@gmail.com>
2018-01-27 20:59:02 -0600

    quicksort working

---
+++ PSort.java
@@ -0,0 +1,17 @@
+//UT-EID=
+
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class PSort {
+  public static void parallelSort(int[] A, int begin, int end){
+	  quickSort QS = new quickSort(A, begin, (end - 1));
+	  ForkJoinPool threadPool = new ForkJoinPool();
+	  threadPool.invoke(QS);
+	  QS.printArray();
+  }
+}
+
+
+
